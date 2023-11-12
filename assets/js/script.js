7f1a6c673b0494976e12bce7c20b335a43a77be5
ilaria <ilariaferrauti@gmail.com>
2023-11-12 21:00:29 +0100

    altre modifiche e tasti frecce

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,14 @@ const btnDoubleSubstraction = document.querySelector(
   "#tasto-double-substraction"
 );
 const display = document.querySelector(".display-number");
+const consoleColor = document.querySelector(".base-console");
 
 //qui seleziono i pulsanti on e reset//
 const btnOn = document.querySelector("#button-start");
 const btnReset = document.querySelector("#button-reset");
 const start = document.querySelector("#start-display");
 const off = document.querySelector("#off-display");
+const schermo = document.querySelector(".schermo-console");
 
 //qui seleziono i pulsanti freccia//
 const arrowTop = document.querySelector("#freccia-su");
@@ -22,58 +24,88 @@ const arrowRight = document.querySelector("#freccia-destra");
 const arrowLeft = document.querySelector("#freccia-sinistra");
 
 let testBtn = false;
+
 function removeStartOff() {
   start.style.display = "none";
   off.style.display = "none";
 }
 
-//evento doppia addizione
-btnDoubleAddiction.addEventListener("click", function () {
+//funzione doppia addizione
+function doppiaAddizione() {
   let numberDisplay = Number(display.textContent);
   display.textContent = numberDisplay + 2;
   removeStartOff();
-});
-
-//evento doppia sottrazione
-btnDoubleSubstraction.addEventListener("click", function () {
+}
+//funzione doppia sottrazione
+function doppiaSottrazione() {
   let numberDisplay = Number(display.textContent);
   display.textContent = numberDisplay - 2;
   removeStartOff();
-});
-
-//evento addizione
-btnAddiction.addEventListener("click", function () {
+}
+//funzione addizione
+function addizione() {
   let numberDisplay = Number(display.textContent);
   display.textContent = numberDisplay + 1;
   removeStartOff();
-});
-//evento sottrazione
-btnSubstraction.addEventListener("click", function () {
+}
+//funzione sottrazione
+function sottrazione() {
   let numberDisplay = Number(display.textContent);
   display.textContent = numberDisplay - 1;
   removeStartOff();
-});
+}
+//funzione tasto reset
+function reset() {
+  display.textContent = 0;
+  consoleColor.style.backgroundColor = "#e9b824";
+  removeStartOff();
+}
 
 //evento tasto on off
 btnOn.addEventListener("click", function () {
+  schermo.classList.toggle("schermo-acceso");
   testBtn = !testBtn;
 
   if (testBtn) {
-    console.log("ON");
-
     start.style.display = "block";
     off.style.display = "none";
-  } else {
-    console.log("OFF");
 
+    btnDoubleAddiction.addEventListener("click", doppiaAddizione);
+
+    btnDoubleSubstraction.addEventListener("click", doppiaSottrazione);
+
+    btnAddiction.addEventListener("click", addizione);
+
+    btnSubstraction.addEventListener("click", sottrazione);
+
+    btnReset.addEventListener("click", reset);
+  } else {
     start.style.display = "none";
     off.style.display = "block";
+
+    btnDoubleAddiction.removeEventListener("click", doppiaAddizione);
+
+    btnDoubleSubstraction.removeEventListener("click", doppiaSottrazione);
+
+    btnAddiction.removeEventListener("click", addizione);
+
+    btnSubstraction.removeEventListener("click", sottrazione);
+
+    btnReset.removeEventListener("click", reset);
   }
   display.textContent = "";
 });
 
-//evento tasto reset
-btnReset.addEventListener("click", function () {
-  display.textContent = 0;
-  removeStartOff();
+//evento frecce
+arrowTop.addEventListener("click", function () {
+  consoleColor.style.backgroundColor = "#FF6D6D";
+});
+arrowBottom.addEventListener("click", function () {
+  consoleColor.style.backgroundColor = "#1572A1";
+});
+arrowLeft.addEventListener("click", function () {
+  consoleColor.style.backgroundColor = "#AE4CCF";
+});
+arrowRight.addEventListener("click", function () {
+  consoleColor.style.backgroundColor = "#2B2730";
 });
