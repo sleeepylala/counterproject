cc66c98d99c94d85ffc26be2091480cb4dd537a7
ilaria <ilariaferrauti@gmail.com>
2023-11-12 19:00:14 +0100

    eventi tasti principali

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,37 @@ const btnDoubleAddiction = document.querySelector("#tasto-double-addiction");
 const btnDoubleSubstraction = document.querySelector(
   "#tasto-double-substraction"
 );
+const display = document.querySelector(".display-number");
 
 //qui seleziono i pulsanti on e reset//
 const btnOn = document.querySelector("#button-start");
 const btnReset = document.querySelector("#button-reset");
 
 //qui seleziono i pulsanti freccia//
-const arrowTop = document.querySelector("#button-start");
+const arrowTop = document.querySelector("#freccia-su");
+const arrowBottom = document.querySelector("#freccia-giù");
+const arrowRight = document.querySelector("#freccia-destra");
+const arrowLeft = document.querySelector("#freccia-sinistra");
+
+//evento doppia addizione
+btnDoubleAddiction.addEventListener("click", function () {
+  let numberDisplay = Number(display.textContent);
+  display.textContent = numberDisplay + 2;
+});
+
+//evento doppia sottrazione
+btnDoubleSubstraction.addEventListener("click", function () {
+  let numberDisplay = Number(display.textContent);
+  display.textContent = numberDisplay - 2;
+});
+
+//evento addizione
+btnAddiction.addEventListener("click", function () {
+  let numberDisplay = Number(display.textContent);
+  display.textContent = numberDisplay + 1;
+});
+//evento sottrazione
+btnSubstraction.addEventListener("click", function () {
+  let numberDisplay = Number(display.textContent);
+  display.textContent = numberDisplay - 1;
+});
