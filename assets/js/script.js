5183c2d7d218e0d05d7bd1325a49461b81dec010
ilaria <ilariaferrauti@gmail.com>
2023-11-12 18:30:42 +0100

    fix tasti principali

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+//qui seleziono i miei pulsanti principali//
+
+const btnAddiction = document.querySelector("#tasto-addiction");
+const btnSubstraction = document.querySelector("#tasto-substraction");
+const btnDoubleAddiction = document.querySelector("#tasto-double-addiction");
+const btnDoubleSubstraction = document.querySelector(
+  "#tasto-double-substraction"
+);
+
+//qui seleziono i pulsanti on e reset//
+const btnOn = document.querySelector("#button-start");
+const btnReset = document.querySelector("#button-reset");
+
+//qui seleziono i pulsanti freccia//
+const arrowTop = document.querySelector("#button-start");
